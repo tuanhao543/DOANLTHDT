5061d1813c28cf1108bd1b15ce1a920bf4c8f869
tuanhao543 <125726528+tuanhao543@users.noreply.github.com>
2023-04-11 12:57:06 +0700

    Update KhoHangDienThoai.java

--- KhoHangDienThoai.java
+++ KhoHangDienThoai.java
@@ -2,29 +2,28 @@ package qlkhodienthoai.doan;
 import java.util.Scanner;
 public class KhoHangDienThoai {
         private Double maKhoHangDienThoai, soLuong;
-        private Float giaBan;
-        private int ngayNhapKho, ngayXuatKho;
+        private Float giaNhap;
+        private int ngayNhapKho;
     public KhoHangDienThoai(){
 
     }
-    public KhoHangDienThoai(String tenDienThoai, Float giaBan, Double soLuong, Double maKhoHangDienThoai, int ngayNhapKho, int ngayXuatKho)
+    public KhoHangDienThoai(String tenDienThoai, Float giaNhap, Double soLuong, Double maKhoHangDienThoai, int ngayNhapKho)
     {
         
-        this.giaBan = giaBan;
+        this.giaBan = giaNhap;
         this.maKhoHangDienThoai= maKhoHangDienThoai;
         this.soLuong= soLuong;
         this.ngayNhapKho= ngayNhapKho;
-        this.ngayXuatKho= ngayXuatKho;
     }    
     
-    public Float getGiaBan() 
+    public Float getGiaNhap() 
     { 
-        return giaBan; 
+        return giaNhap; 
     } 
  
-    public void setGiaBan(Float giaBan ) 
+    public void setGiaNhap(Float giaNhap ) 
     { 
-        this.giaBan = giaBan; 
+        this.giaNhap = giaNhap; 
     }
     public Double getMaKhoHangDienThoai()
         {
@@ -50,28 +49,18 @@ public class KhoHangDienThoai {
         {
         this.ngayNhapKho = ngayNhapKho;  
         }
-    public int getNgayXuatKho()
-        {
-        return ngayXuatKho;
-        }
-    public void setNgayXuatKho(int ngayXuatKho)
-        {
-        this.ngayXuatKho = ngayXuatKho;
-        }
     public void nhapKhoHangDienThoai(){
         Scanner sc = new Scanner(System.in);
         System.out.println("Mã kho hàng điện thoại: ");
         maKhoHangDienThoai = sc.nextDouble();
-        System.out.println("Giá bán");
+        System.out.println("Giá nhập");
         giaBan=sc.nextFloat();
         System.out.println("Số lượng: ");
         soLuong = sc.nextDouble();
         System.out.println("Ngày nhập kho: ");
         ngayNhapKho = sc.nextInt();
-        System.out.println("Ngày xuất kho:");
-        ngayXuatKho = sc.nextInt();
     }
     public void hienKhoHangDienThoai(){
-        System.out.println("\nMa Kho Hang Dien Thoai:"+getMaKhoHangDienThoai()+"\nGia Ban:"+ getGiaBan()+"\nSo Luong:"+getSoLuong()+"\nNgay Nhap Kho:"+getNgayNhapKho()+"\nNgay Xuat Kho:"+getNgayXuatKho());
+        System.out.println("\nMa Kho Hang Dien Thoai:"+getMaKhoHangDienThoai()+"\nGia Nhap:"+ getGiaNhap()+"\nSo Luong:"+getSoLuong()+"\nNgay Nhap Kho:"+getNgayNhapKho());
     }
  }
